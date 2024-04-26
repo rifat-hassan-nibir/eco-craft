3c48075147e09d2cc4b401bcca073b9a26ee0f08
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2024-04-26 12:53:37 +0600

    added navlink and padding to mobile menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,13 +10,19 @@ const Navbar = () => {
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
             </svg>
           </div>
-          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-            <li>
-              <a>Home</a>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
+          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[20] p-4 shadow bg-base-100 rounded-box w-52">
+            <NavLink to="/" className={({ isActive }) => (isActive ? "text-primary font-bold py-1" : "py-1")}>
+              Home
+            </NavLink>
+            <NavLink to="/all-art-and-craft-items" className={({ isActive }) => (isActive ? "text-primary font-bold py-1" : "py-1")}>
+              All Art & Craft Items
+            </NavLink>
+            <NavLink to="/add-craft-item" className={({ isActive }) => (isActive ? " text-primary font-bold py-1" : "py-1")}>
+              Add Craft Item
+            </NavLink>
+            <NavLink to="/my-art-and-craft-list" className={({ isActive }) => (isActive ? " text-primary font-bold py-1" : "py-1")}>
+              My Art & Craft List
+            </NavLink>
           </ul>
         </div>
         <Link to="/" className="text-xl font-bold">
@@ -24,7 +30,7 @@ const Navbar = () => {
         </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1 items-center gap-3">
+        <ul className="menu menu-horizontal px-1 items-center gap-5">
           <NavLink to="/" className={({ isActive }) => (isActive ? "text-primary font-bold" : "")}>
             Home
           </NavLink>
